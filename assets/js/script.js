c9a26eb644abb763397e1b0c3ad5fe46c7d94812
Jordan Ross <jorross@umich.edu>
2022-02-07 15:35:31 -0800

    set up a geocode fetch request. Still need to add user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-var url_path = "https://maps.googleapis.com/maps/api/geocode/json"
-var url_params = "?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+var geo_url_path = "https://maps.googleapis.com/maps/api/geocode/json"
+var geo_url_params = "?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
 var api_key = "&key=AIzaSyCAhY-AP5wzYt1ngWZ86qHYzoUsYKnoQmE";
 
-var requestUrl = url_path.concat(url_params, api_key);
+var geoRequestUrl = geo_url_path.concat(geo_url_params, api_key);
 
 var lat, lng;
 
@@ -16,11 +16,11 @@ function callApi(requestUrl) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data.results[0].geometry.location);
+        // console.log(data.results[0].geometry.location);
         lat = data.results[0].geometry.location.lat;
         lng = data.results[0].geometry.location.lng;
         console.log("Coordinates: (" + lat + ", " + lng + ")")
     });
   }
   
-callApi(requestUrl);
+callApi(geoRequestUrl);
