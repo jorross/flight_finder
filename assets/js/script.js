6d703d84e0ddb17d17a8a59440a5b82bd0df68ff
Jaylen Taylor <96092354+ultimatejayfox@users.noreply.github.com>
2022-02-07 18:54:22 -0500

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var geo_url_path = "https://maps.googleapis.com/maps/api/geocode/json"
+var geo_url_params = "?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+var api_key = "&key=AIzaSyCAhY-AP5wzYt1ngWZ86qHYzoUsYKnoQmE";
+
+var geoRequestUrl = geo_url_path.concat(geo_url_params, api_key);
+
+var lat, lng;
+
+function callApi(requestUrl) {
+    fetch(requestUrl)
+      .then(function (response) {
+        // console.log(response);
+        // if (response.status === 200) {
+        //   responseText.textContent = response.status;
+        // }
+        return response.json();
+    })
+    .then(function (data) {
+        // console.log(data.results[0].geometry.location);
+        lat = data.results[0].geometry.location.lat;
+        lng = data.results[0].geometry.location.lng;
+        console.log("Coordinates: (" + lat + ", " + lng + ")")
+    });
+  }
+  
+callApi(geoRequestUrl);
