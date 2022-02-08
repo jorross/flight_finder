4f461239c5968b571c32cdb3a12b7a027b21ea3b
Jordan Ross <jorross@umich.edu>
2022-02-07 16:14:24 -0800

    saving work

---
+++ assets/js/flight.js
@@ -0,0 +1,6 @@
+window.initMap = function() {
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+        });
+}
