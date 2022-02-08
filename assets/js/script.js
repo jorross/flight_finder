a75cbf7878c1bd45c04a08fab07400afde03d33f
Jordan Ross <jorross@umich.edu>
2022-02-07 18:08:43 -0800

    successfully geocoding user inputted locations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,65 @@
 var geo_url_path = "https://maps.googleapis.com/maps/api/geocode/json"
-var geo_url_params = "?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+var geo_url_params = "?address="
+var geo_url_params2 = "?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
 var api_key = "&key=AIzaSyCAhY-AP5wzYt1ngWZ86qHYzoUsYKnoQmE";
 
-var geoRequestUrl = geo_url_path.concat(geo_url_params, api_key);
+var geoRequestUrl = geo_url_path.concat(geo_url_params2, api_key);
 
 var lat, lng;
-
-function callApi(requestUrl) {
-    fetch(requestUrl)
-      .then(function (response) {
-        // console.log(response);
-        // if (response.status === 200) {
-        //   responseText.textContent = response.status;
-        // }
-        return response.json();
-    })
-    .then(function (data) {
-        // console.log(data.results[0].geometry.location);
-        lat = data.results[0].geometry.location.lat;
-        lng = data.results[0].geometry.location.lng;
-        console.log("Coordinates: (" + lat + ", " + lng + ")")
-    });
-  }
-  
-callApi(geoRequestUrl);
 
 let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.422388, lng: -122.0841883 },
+    center: { lat: 34.0848304, lng: -84.3893775 },
     zoom: 8,
   });
 }
+
+function callApi(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {
+      // console.log(response);
+      // if (response.status === 200) {
+      //   responseText.textContent = response.status;
+      // }
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data.results[0].geometry.location);
+      lat = data.results[0].geometry.location.lat;
+      lng = data.results[0].geometry.location.lng;
+      console.log("Coordinates: (" + lat + ", " + lng + ")/n")
+    });
+};
+
+$('#form').on('click', "#searchBtn", function (event) {
+  event.preventDefault();
+  var origin_input = $('#from').val();
+  var stringSplit2 = origin_input.split(" ");
+  for (var i = 0; i < stringSplit2.length; i++) {
+    if (i < stringSplit2.length - 1) {
+      geo_url_params += stringSplit2[i] + "+";
+    }
+    else {
+      geo_url_params += stringSplit2[i]
+    }
+  };
+  callApi(geo_url_path+geo_url_params+api_key);
+
+  var dest_input = $('#destination').val();
+  var stringSplit = dest_input.split(" ");
+  for (var i = 0; i < stringSplit.length; i++) {
+    if (i < stringSplit.length - 1) {
+      geo_url_params += stringSplit[i] + "+";
+    }
+    else {
+      geo_url_params += stringSplit[i]
+    }
+  };
+  console.log(geo_url_params);
+  // console.log(geoRequestUrl);
+  // console.log(geo_url_path+geo_url_params+api_key)
+  callApi(geo_url_path+geo_url_params+api_key);
+}
+)
