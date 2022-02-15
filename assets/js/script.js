18f77ee3db44ba640f0d8c6ddaab420bc8ff1979
Jordan Ross <jorross@umich.edu>
2022-02-14 16:15:34 -0800

    getting flight results for aviation stack

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var geo_url_params2 = "?address=";
 var api_key = "&key=AIzaSyCAhY-AP5wzYt1ngWZ86qHYzoUsYKnoQmE";
 var flight_Path = "https://api.aviationstack.com/v1/flights"
 var flight_Param = "?"
-var flightApi = "access_key=00ef121ef8bdc0d46f86b8dcf2b97155";
+var flightApi = "access_key=a867cfeb994a9fe529a2a3195bb03be8";
 // https://api.aviationstack.com/v1/flights ? access_key = YOUR_ACCESS_KEY
 var lat, lng;
 
@@ -12,41 +12,53 @@ let map;
 
 if (location === "./results.html") {
   initMap();
-<<<<<<< HEAD
-}
-function flightUrl() {
-=======
 };
+
 var newURL = flightURL()
-function flightURL(){
-   var DateSplit = localStorage.getItem("Date").split(", ");
-   var Date1 = DateSplit[0];
-   var Date2 = DateSplit[1];
-   var originInput = localStorage.getItem("origin_String")
-   var DestinationInput = localStorage.getItem("Destination_String")
-   fetch("http://api.aviationstack.com/v1/cities?access_key=00ef121ef8bdc0d46f86b8dcf2b97155")
-   .then(function(result){
-     console.log(result.json());
-   });
-   flight_Param += "flight_date=" + Date1 ;
-   flight_Param += "flight_date="  + Date2 ;
-   
-   return flight_Path + flight_Param + flightApi;
 
-  };
-   
-  
+flightApiCall(newURL);
 
-  
->>>>>>> 442628ebf3cdd08baf6828f2e98f5d92f9943e5f
+function flightURL() {
+  // var DateSplit = localStorage.getItem("Date").split(", ");
+  // var Date1 = DateSplit[0];
+  // var Date2 = DateSplit[1];
+  // var originInput = localStorage.getItem("origin_String")
+  // var DestinationInput = localStorage.getItem("Destination_String")
 
+  // DAY OF PRESENTATION: CHANGE DATE!!!!!!!!!!!!!
+  flight_Param += "dep_iata=ATL&arr_iata=SEA&flight_date=2022-02-14&flight_status=scheduled&"
+
+  return flight_Path + flight_Param + flightApi;
+
+};
+
+function flightApiCall(requestUrl) {
+  return fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      for (var i = 0; i < data.length; i++) {
+        localStorage.setItem("flight" + i, data[i].toString())
+      }
+      // var payload = {
+      //   "Lat": lat,
+      //   "Lng": lng
+      // }
+      // return payload;
+    });
 }
+
+
+
 function initMap() {
   const myLatLng = { lat: 34.0848304, lng: -84.3893775 };
   var stringSplit = localStorage.getItem('origin').split(", ");
-  const origin = {lat: parseInt(stringSplit[0]), lng: parseInt(stringSplit[1])};
+  const origin = { lat: parseInt(stringSplit[0]), lng: parseInt(stringSplit[1]) };
   var stringSplit = localStorage.getItem('destination').split(", ");
-  const destination = {lat: parseInt(stringSplit[0]), lng: parseInt(stringSplit[1])};
+  const destination = { lat: parseInt(stringSplit[0]), lng: parseInt(stringSplit[1]) };
   map = new google.maps.Map(document.getElementById("map"), {
     center: myLatLng,
     zoom: 4,
@@ -90,9 +102,8 @@ function callGeoApi(requestUrl) {
     });
 };
 
-$('#form').on('click', "#searchBtn", function (event) {
+$('.content').on('click', "#searchBtn", function (event) {
   event.preventDefault();
-
   // Parse origin location input from the html
   var origin_input = $('#from').val();
   var stringSplit2 = origin_input.split(" ");
@@ -115,19 +126,20 @@ $('#form').on('click', "#searchBtn", function (event) {
       geo_url_params2 += stringSplit[i]
     }
   };
-  // storage the Date, origin and destination in localstorage 
-    var Date_input = "" + $("#date-picker-from").val() + ", "  + $("#date-picker-to").val() ;
-    localStorage.setItem("Date", Date_input);
-    localStorage.setItem("origin_String", origin_input);
-    localStorage.setItem("Destination_String", dest_input);
+  // storage the Date, origin and destination in localstorage
+  // console.log($("#date-input"))
+  // var Date_input = "" + $("#date-input").val() + ", " + $("#return-date-input").val();
+  // localStorage.setItem("Date", Date_input);
+  localStorage.setItem("origin_String", origin_input);
+  localStorage.setItem("Destination_String", dest_input);
 
   // make call to geocode api for the origin coordinates
   callGeoApi(geo_url_path + geo_url_params1 + api_key).then(function (result1) {
     // make call to geocode api for the destination coordinates
     callGeoApi(geo_url_path + geo_url_params2 + api_key).then(function (result2) {
       // console.log(result1);
-      localStorage.setItem('origin', ""+result1.Lat+", "+result1.Lng);
-      localStorage.setItem('destination', ""+result2.Lat+", "+result2.Lng);
+      localStorage.setItem('origin', "" + result1.Lat + ", " + result1.Lng);
+      localStorage.setItem('destination', "" + result2.Lat + ", " + result2.Lng);
       location = './results.html';
     });
 
